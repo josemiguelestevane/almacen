ddc9b515aa00e024234980c466c53039e74a8940
USUARIO <USUARIO@DESKTOP-3E20268>
2019-04-16 11:12:30 -0500

    cambio login

---
+++ src/clases/main.java
@@ -0,0 +1,27 @@
+package clases;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import frames.login.login;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    login principal=new login();
+    principal.setVisible(true);
+    }
+    
+}
